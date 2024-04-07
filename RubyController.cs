d9f743bdd9875adacf5acb4f0a2a050bfb3fb571
Skrycer <ciceroserre@gmail.com>
2024-04-07 20:24:21 +0100

    toturial colecionaveis feito

--- RubyController.cs
+++ RubyController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -12,6 +17,9 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
     // Update is called once per frame
     void Update()
@@ -20,11 +28,17 @@ public class RubyController : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
     }
     void FixedUpdate()
-    { 
+    {
         Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        
+
         rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    
     }
 }
