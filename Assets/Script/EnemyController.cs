e93f3e39bc6dbff2f14c281e597f029ed0816e90
Skrycer <ciceroserre@gmail.com>
2024-05-19 21:40:33 +0100

    add speed aos animais

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -11,6 +11,7 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+
     Animator animator;
 
     // Start is called before the first frame update
@@ -30,17 +31,6 @@ public class EnemyController : MonoBehaviour
             direction = -direction;
             timer = changeTime;
         }
-        if (vertical)
-        {
-            animator.SetFloat("Move X", 0);
-            animator.SetFloat("Move Y", direction);
-        }
-
-        else 
-        { 
-            animator.SetFloat("Move X", direction);
-            animator.SetFloat("Move Y", 0);
-        }
     }
 
     void FixedUpdate()
@@ -49,11 +39,15 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction; ;
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction; ;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2D.MovePosition(position);
